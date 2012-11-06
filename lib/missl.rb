28f844e9e3411e44e3495d32e05d4976901dbc36
Kramer Campbell <kramer@vexsoftware.com>
2012-11-05 18:28:26 -0800

    Correct certain issues.
    Version was mislocated and not referenced properly from the Gemspec. A
    blank file that was left is now deleted. The Sinatra app is now wrapped
    in a class so that it can be mounted.

--- lib/missl.rb
+++ lib/missl.rb
@@ -1,31 +1,40 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'net/http'
 require 'uri'
-require 'missi/version'
+require 'missl/version'
 
 RESTRICTED_IPS = /^((10\.)|(127\.)|(169\.254)|(192\.168)|(172\.((1[6-9])|(2[0-9])|(3[0-1]))))/
 
-get '/' do
-  # Filter restricted IPs.
-  if params[:uri].match(RESTRICTED_IPS)
-    return 403
+module Missl
+  class Base < Sinatra::Base
+    get '/' do
+      # Make sure there is a URL.
+      if params[:url].nil? || params[:url].empty?
+        return 404
+      end
+
+      # Filter restricted IPs.
+      if params[:url].match(RESTRICTED_IPS)
+        return 403
+      end
+
+      # Make the request.
+      resp = Net::HTTP.get_response(URI.parse(params[:url]))
+
+      # Filter the content type.
+      if resp['content-type'].slice(0, 5) != 'image'
+        return 404
+      end
+
+      # Filter the content length.
+      if resp['content-length'].to_i > 5242880
+        return 404
+      end
+
+      # All done.
+      response.headers['content-length'] = resp['content-length']
+      response.headers['content-type'] = resp['content-type']
+      return resp.body
+    end
   end
-
-  # Make the request.
-  resp = Net::HTTP.get_response(URI.parse(params[:uri]))
-
-  # Filter the content type.
-  if resp['content-type'].slice(0, 5) != 'image'
-    return 404
-  end
-
-  # Filter the content length.
-  if resp['content-length'].to_i > 5242880
-    return 404
-  end
-
-  # All done.
-  response.headers['content-length'] = resp['content-length']
-  response.headers['content-type'] = resp['content-type']
-  return resp.body
 end
