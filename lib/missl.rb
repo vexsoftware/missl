1521208c2e56ba4ddf5529d1826c831b800afea6
Kramer Campbell <kramer@vexsoftware.com>
2012-11-05 19:47:13 -0800

    Add redirect handling.

--- lib/missl.rb
+++ lib/missl.rb
@@ -4,9 +4,22 @@ require 'uri'
 require 'missl/version'
 
 RESTRICTED_IPS = /^((10\.)|(127\.)|(169\.254)|(192\.168)|(172\.((1[6-9])|(2[0-9])|(3[0-1]))))/
+REDIRECT_LIMIT = 5
 
 module Missl
   class Base < Sinatra::Base
+    def fetch(uri, redirect_limit)
+      raise ArgumentError, "Too many redirects" unless redirect_limit > 0
+      
+      resp = Net::HTTP.get_response(URI.parse(uri))
+      case resp
+      when Net::HTTPSuccess then resp
+      when Net::HTTPRedirection then fetch(resp['location'], redirect_limit - 1)
+      else
+        resp.error!
+      end
+    end
+
     get '/' do
       # Make sure there is a URL.
       if params[:url].nil? || params[:url].empty?
@@ -19,7 +32,7 @@ module Missl
       end
 
       # Make the request.
-      resp = Net::HTTP.get_response(URI.parse(params[:url]))
+      resp = fetch(params[:url], REDIRECT_LIMIT)
 
       # Filter the content type.
       if resp['content-type'].slice(0, 5) != 'image'
